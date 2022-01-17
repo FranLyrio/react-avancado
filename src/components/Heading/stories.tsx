c3bd19ba5451959d58cdf96a85dc02565e61cc7b
FranLyrio <lyriofranciele@gmail.com>
2022-01-17 19:24:55 -0300

    feat: componente Heading

---
+++ src/components/Heading/stories.tsx
@@ -0,0 +1,16 @@
+import { Meta, Story } from '@storybook/react';
+import Heading, { HeadingProps } from '.';
+
+export default {
+	title: 'Heading',
+	component: Heading,
+	argTypes: {
+		children: {
+			type: 'string'
+		}
+	}
+} as Meta;
+
+export const Basic: Story<HeadingProps> = (args) => (
+	<Heading {...args}>Most Populars</Heading>
+);
