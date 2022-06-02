2186ac1f229105e9a16e204ebbd6caf1d5d8f26c
FranLyrio <lyriofranciele@gmail.com>
2022-06-02 09:19:40 -0300

    test(Slider): cria testes para o funcionamento do componente

--- src/components/Slider/test.spec.tsx
+++ src/components/Slider/test.spec.tsx
@@ -7,12 +7,18 @@ import Slider from '.'
 describe('<Slider />', () => {
 	it('should render children as slider item', () => {
 		renderWithTheme(
-			<Slider settings={{ slidesToShow: 2 }}>
+			<Slider settings={{ slidesToShow: 1, infinite: false }}>
 				<p>Item 1</p>
-				<p>Item 1</p>
+				<p>Item 2</p>
 			</Slider>
 		)
 
-		expect
+		expect(
+			screen.getByText(/item 1/i).parentElement?.parentElement
+		).toHaveClass('slick-slide')
+
+		expect(
+			screen.getByText(/item 2/i).parentElement?.parentElement
+		).toHaveClass('slick-slide')
 	})
 })
