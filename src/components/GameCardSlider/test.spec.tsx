a67bb38f919df43e8c2296a15daf6b307a9c53ac
FranLyrio <lyriofranciele@gmail.com>
2022-06-02 19:34:30 -0300

    feat: adiciona componente slider com os game cards

---
+++ src/components/GameCardSlider/test.spec.tsx
@@ -0,0 +1,72 @@
+import { screen } from '@testing-library/react'
+import '../../../.jest/match-media-mock'
+import { renderWithTheme } from '../../utils/tests/helpers'
+
+import GameCardSlider from '.'
+
+const items = [
+	{
+		title: 'Population Zero',
+		developer: 'Rockstar Games',
+		img: 'https://source.unsplash.com/user/willianjusten/300x140',
+		price: 'R$ 235,00',
+		promotionalPrice: 'R$ 215,00'
+	},
+	{
+		title: 'Population Zero',
+		developer: 'Rockstar Games',
+		img: 'https://source.unsplash.com/user/willianjusten/300x141',
+		price: 'R$ 235,00',
+		promotionalPrice: 'R$ 215,00'
+	},
+	{
+		title: 'Population Zero',
+		developer: 'Rockstar Games',
+		img: 'https://source.unsplash.com/user/willianjusten/300x142',
+		price: 'R$ 235,00',
+		promotionalPrice: 'R$ 215,00'
+	},
+	{
+		title: 'Population Zero',
+		developer: 'Rockstar Games',
+		img: 'https://source.unsplash.com/user/willianjusten/300x143',
+		price: 'R$ 235,00',
+		promotionalPrice: 'R$ 215,00'
+	},
+	{
+		title: 'Population Zero',
+		developer: 'Rockstar Games',
+		img: 'https://source.unsplash.com/user/willianjusten/300x144',
+		price: 'R$ 235,00',
+		promotionalPrice: 'R$ 215,00'
+	}
+]
+
+describe('<GameSlider />', () => {
+	it('should render with 4 active items', () => {
+		const { container } = renderWithTheme(<GameCardSlider items={items} />)
+		expect(container.querySelectorAll('.slick-active')).toHaveLength(4)
+	})
+
+	it('should render back arrows by default', () => {
+		renderWithTheme(<GameCardSlider items={items} />)
+
+		expect(screen.getByLabelText(/previous games/i)).toHaveStyle({
+			color: '#030517'
+		})
+		expect(screen.getByLabelText(/next games/i)).toHaveStyle({
+			color: '#030517'
+		})
+	})
+
+	it('should render white arrows if color passed', () => {
+		renderWithTheme(<GameCardSlider items={items} arrowColor="white" />)
+
+		expect(screen.getByLabelText(/previous games/i)).toHaveStyle({
+			color: '#FAFAFA'
+		})
+		expect(screen.getByLabelText(/next games/i)).toHaveStyle({
+			color: '#FAFAFA'
+		})
+	})
+})
