e29d45e8d0f6b124327935c153d31e55e6084cbd
FranLyrio <lyriofranciele@gmail.com>
2022-06-04 17:12:09 -0300

    refactor(Home): adiciona propriedades do template home

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -1,20 +1,51 @@
+import { GameCardProps } from '../../components/GameCard'
+import { BannerProps } from '../../components/Banner'
+import { HighlightProps } from '../../components/Highlight'
+
 import { Container } from '../../components/Container'
 import Menu from '../../components/Menu'
 import Footer from '../../components/Footer'
 import Heading from '../../components/Heading'
+import BannerSlider from '../../components/BannerSlider'
+import GameCardSlider from '../../components/GameCardSlider'
 
 import * as S from './styles'
 
-const Home = () => (
+export type HomeTemplateProps = {
+	banners: BannerProps[]
+	newGames: GameCardProps[]
+	mostPopularHighlight: HighlightProps
+	mostPopularGames: GameCardProps[]
+	upcomingGames: GameCardProps[]
+	upcomingHighligth: HighlightProps
+	upcomingMoreGames: GameCardProps[]
+	freeGames: GameCardProps[]
+	freeHighligth: HighlightProps
+}
+
+const Home = ({
+	banners,
+	freeGames,
+	freeHighligth,
+	mostPopularGames,
+	mostPopularHighlight,
+	newGames,
+	upcomingGames,
+	upcomingHighligth,
+	upcomingMoreGames
+}: HomeTemplateProps) => (
 	<section>
 		<Container>
 			<Menu />
+			<BannerSlider items={banners} />
 		</Container>
 
 		<Container>
 			<Heading lineLeft lineColor="secondary" color="black">
 				News
 			</Heading>
+
+			<GameCardSlider items={newGames} />
 		</Container>
 
 		<Container>
