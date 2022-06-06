b80f26c6107e24bbf7fd5f2b4c03b00183b38a00
FranLyrio <lyriofranciele@gmail.com>
2022-06-06 10:33:07 -0300

    feat(Home): cria página Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,24 @@
-import Home from 'templates/Home'
+import Home, { HomeTemplateProps } from 'templates/Home'
+import bannersMock from 'components/BannerSlider/mock'
+import gamesMock from 'components/GameCardSlider/mock'
+import highLightMock from 'components/Highlight/mock'
 
-export default function Index() {
-	return <Home />
+export default function Index(props: HomeTemplateProps) {
+	return <Home {...props} />
+}
+
+export function getServerSideProps() {
+	return {
+		props: {
+			banners: bannersMock,
+			newGames: gamesMock,
+			mostPopularHighlight: highLightMock,
+			mostPopularGames: gamesMock,
+			upcomingGames: gamesMock,
+			upcomingHighligth: highLightMock,
+			upcomingMoreGames: gamesMock,
+			freeGames: gamesMock,
+			freeHighligth: highLightMock
+		}
+	}
 }
