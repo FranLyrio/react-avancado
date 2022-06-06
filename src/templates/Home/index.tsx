87bbf77232c63860232e9755ac9e30a1db2569f7
FranLyrio <lyriofranciele@gmail.com>
2022-06-06 10:32:42 -0300

    feat(Home): adiciona template para a página home

--- src/templates/Home/index.tsx
+++ src/templates/Home/index.tsx
@@ -8,6 +8,7 @@ import Footer from '../../components/Footer'
 import Heading from '../../components/Heading'
 import BannerSlider from '../../components/BannerSlider'
 import GameCardSlider from '../../components/GameCardSlider'
+import Highlight from '../../components/Highlight'
 
 import * as S from './styles'
 
@@ -37,38 +38,52 @@ const Home = ({
 	<section>
 		<Container>
 			<Menu />
-			<BannerSlider items={banners} />
+			<S.SectionBanner>
+				<BannerSlider items={banners} />
+			</S.SectionBanner>
 		</Container>
+
+		<S.SectionNews>
+			<Container>
+				<Heading lineLeft lineColor="secondary">
+					News
+				</Heading>
+				<GameCardSlider items={newGames} arrowColor="black" />
+			</Container>
+		</S.SectionNews>
 
 		<Container>
-			<Heading lineLeft lineColor="secondary" color="black">
-				News
-			</Heading>
+			<S.SectionMostPopular>
+				<Heading lineLeft lineColor="secondary">
+					Most Popular
+				</Heading>
+				<Highlight {...mostPopularHighlight} />
+				<GameCardSlider items={mostPopularGames} />
+			</S.SectionMostPopular>
 
-			<GameCardSlider items={newGames} />
+			<S.SectionUpcoming>
+				<Heading lineLeft lineColor="secondary">
+					Upcoming
+				</Heading>
+				<GameCardSlider items={upcomingGames} />
+				<Highlight {...upcomingHighligth} />
+				<GameCardSlider items={upcomingMoreGames} />
+			</S.SectionUpcoming>
+
+			<S.SectionFreeGames>
+				<Heading lineLeft lineColor="secondary">
+					Free games
+				</Heading>
+				<Highlight {...freeHighligth} />
+				<GameCardSlider items={freeGames} />
+			</S.SectionFreeGames>
 		</Container>
 
-		<Container>
-			<Heading lineLeft lineColor="secondary">
-				Most Popular
-			</Heading>
-		</Container>
-
-		<Container>
-			<Heading lineLeft lineColor="secondary">
-				Upcoming
-			</Heading>
-		</Container>
-
-		<Container>
-			<Heading lineLeft lineColor="secondary">
-				Free Games
-			</Heading>
-		</Container>
-
-		<Container>
-			<Footer />
-		</Container>
+		<S.SectionFooter>
+			<Container>
+				<Footer />
+			</Container>
+		</S.SectionFooter>
 	</section>
 )
 
--- src/templates/Home/test.spec.tsx
+++ src/templates/Home/test.spec.tsx
@@ -1,20 +1,37 @@
+import '../../../.jest/match-media-mock'
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from '../../utils/tests/helpers'
 
+import bannerMock from '../../components/BannerSlider/mock'
+import gamesMock from '../../components/GameCardSlider/mock'
+import highlightMock from '../../components/Highlight/mock'
+
 import Home from '.'
+
+const props = {
+	banners: bannerMock,
+	newGames: [gamesMock[0]],
+	mostPopularHighlight: highlightMock,
+	mostPopularGames: [gamesMock[0]],
+	upcomingGames: [gamesMock[0]],
+	upcomingHighligth: highlightMock,
+	upcomingMoreGames: [gamesMock[0]],
+	freeGames: [gamesMock[0]],
+	freeHighligth: highlightMock
+}
 
 describe('<Home />', () => {
 	it('should render menu and footer', () => {
-		renderWithTheme(<Home />)
+		renderWithTheme(<Home {...props} />)
 
 		expect(screen.getByLabelText(/open menu/i)).toBeInTheDocument()
 		expect(
-			screen.getByRole('heading', { name: /contact us/i })
+			screen.getByRole('heading', { name: /follow us/i })
 		).toBeInTheDocument()
 	})
 
 	it('should render the headings', () => {
-		renderWithTheme(<Home />)
+		renderWithTheme(<Home {...props} />)
 
 		expect(screen.getByRole('heading', { name: /news/i })).toBeInTheDocument()
 		expect(
@@ -26,5 +43,15 @@ describe('<Home />', () => {
 		expect(
 			screen.getByRole('heading', { name: /free games/i })
 		).toBeInTheDocument()
+	})
+
+	it('should render section elements', () => {
+		renderWithTheme(<Home {...props} />)
+		// banner
+		expect(screen.getAllByText(/defy death 1/i)).toHaveLength(1)
+		// card game (5 sections com 4 cards cada = 5x4 = 20; apenas um card 5x1)
+		expect(screen.getAllByText(/population zero/i)).toHaveLength(5)
+		// highlight
+		expect(screen.getAllByText(/read dead it’s back/i)).toHaveLength(3)
 	})
 })
