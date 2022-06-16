e71aa68823d873202132896c181a2ff0b6d25c84
FranLyrio <lyriofranciele@gmail.com>
2022-06-16 19:43:11 -0300

    refactor(GameDetails): adiciona mais propriedades e cria mock

--- src/components/GameDetails/index.tsx
+++ src/components/GameDetails/index.tsx
@@ -2,21 +2,39 @@ import { Apple, Windows, Linux } from '@styled-icons/fa-brands'
 
 import Heading from 'components/Heading'
 import MediaMatch from 'components/MediaMatch'
+import { useMemo } from 'react'
 
 import * as S from './styles'
 
 type Platform = 'windows' | 'linux' | 'mac'
 
 export type GameDetailsProps = {
+	developer: string
 	platforms: Platform[]
+	releaseDate: string
 }
 
-const GameDetails = ({ platforms }: GameDetailsProps) => {
+const GameDetails = ({
+	platforms,
+	developer,
+	releaseDate
+}: GameDetailsProps) => {
 	const platformIcons = {
 		linux: <Linux title="Linux" size={18} />,
 		windows: <Windows title="Windows" size={18} />,
 		mac: <Apple title="Mac" size={18} />
 	}
+
+	const formattedReleaseDate = useMemo(() => {
+		const dateFormat = new Intl.DateTimeFormat('en-US', {
+			day: 'numeric',
+			month: 'short',
+			year: 'numeric'
+		})
+		const formattedDate = dateFormat.format(new Date(releaseDate))
+
+		return formattedDate
+	}, [releaseDate])
 
 	return (
 		<S.Wrapper>
@@ -29,12 +47,12 @@ const GameDetails = ({ platforms }: GameDetailsProps) => {
 			<S.Content>
 				<S.Block>
 					<S.Label>Developer</S.Label>
-					<S.Description>Gearbox Software</S.Description>
+					<S.Description>{developer}</S.Description>
 				</S.Block>
 
 				<S.Block>
 					<S.Label>Release Date</S.Label>
-					<S.Description>Nov 16, 2019</S.Description>
+					<S.Description>{formattedReleaseDate}</S.Description>
 				</S.Block>
 
 				<S.Block>
