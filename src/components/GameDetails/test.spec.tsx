ff972a13f0f0eb5bb74b560ab3449f97085ce7e4
FranLyrio <lyriofranciele@gmail.com>
2022-06-16 19:43:54 -0300

    test(GameDetails): testa formatação de data e seções

--- src/components/GameDetails/test.spec.tsx
+++ src/components/GameDetails/test.spec.tsx
@@ -1,14 +1,48 @@
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
-import GameDetails from '.'
+import mock from './mock'
+
+import GameDetails, { GameDetailsProps } from '.'
+
+const props = mock as GameDetailsProps
 
 describe('<GameDetails />', () => {
-	it('should render the heading', () => {
-		renderWithTheme(<GameDetails />)
+	it('should render the blocks', () => {
+		renderWithTheme(<GameDetails {...props} />)
 
-		const heading = screen.getByRole('heading', { name: /GameDetails/i })
+		expect(
+			screen.getByRole('heading', { name: /developer/i })
+		).toBeInTheDocument()
 
-		expect(heading).toBeInTheDocument()
+		expect(
+			screen.getByRole('heading', { name: /release date/i })
+		).toBeInTheDocument()
+
+		expect(
+			screen.getByRole('heading', { name: /platforms/i })
+		).toBeInTheDocument()
+
+		expect(
+			screen.getByRole('heading', { name: /publisher/i })
+		).toBeInTheDocument()
+
+		expect(screen.getByRole('heading', { name: /rating/i })).toBeInTheDocument()
+
+		expect(screen.getByRole('heading', { name: /genres/i })).toBeInTheDocument()
+	})
+
+	it('should render platform icons', () => {
+		renderWithTheme(<GameDetails {...props} />)
+
+		expect(screen.getByRole('img', { name: /linux/i })).toBeInTheDocument()
+		expect(screen.getByRole('img', { name: /windows/i })).toBeInTheDocument()
+		expect(screen.getByRole('img', { name: /mac/i })).toBeInTheDocument()
+	})
+
+	it('should render formatted date', () => {
+		renderWithTheme(<GameDetails {...props} />)
+
+		expect(screen.getByText('Nov 21, 2020')).toBeInTheDocument()
 	})
 })
