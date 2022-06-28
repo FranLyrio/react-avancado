390cae47a068c10ebe37eb6798647f186e9c84fb
FranLyrio <lyriofranciele@gmail.com>
2022-06-28 10:52:11 -0300

    feat(Game): adiciona componente de galeria ao template

--- src/templates/Game/index.tsx
+++ src/templates/Game/index.tsx
@@ -1,4 +1,5 @@
 import GameInfo, { GameInfoProps } from 'components/GameInfo'
+import Gallery, { GalleryImageProps } from 'components/Gallery'
 import Base from 'templates/Base'
 
 import * as S from './styles'
@@ -6,14 +7,19 @@ import * as S from './styles'
 export type GameTemplateProps = {
 	cover: string
 	gameInfo: GameInfoProps
+	gallery?: GalleryImageProps[]
 }
 
-const Game = ({ cover, gameInfo }: GameTemplateProps) => (
+const Game = ({ cover, gameInfo, gallery }: GameTemplateProps) => (
 	<Base>
 		<S.Cover src={cover} role="image" aria-label="cover" />
 		<S.Main>
 			<S.SectionGameInfo>
 				<GameInfo {...gameInfo} />
+			</S.SectionGameInfo>
+
+			<S.SectionGameInfo>
+				{!!gallery && <Gallery items={gallery} />}
 			</S.SectionGameInfo>
 		</S.Main>
 	</Base>
