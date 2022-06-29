1917976af14ea77a5c93bcf236837f90a6e5c322
FranLyrio <lyriofranciele@gmail.com>
2022-06-29 20:40:24 -0300

    feat(Wishlist): adiciona template para página

--- src/templates/Wishlist/index.tsx
+++ src/templates/Wishlist/index.tsx
@@ -1,27 +1,40 @@
 import Base from 'templates/Base'
 
 import Heading from 'components/Heading'
+import Showcase from 'components/Showcase'
 import { Container } from 'components/Container'
-import { GameCardProps } from 'components/GameCard'
+import GameCard, { GameCardProps } from 'components/GameCard'
 import { HighlightProps } from 'components/Highlight'
 
 import * as S from './styles'
 
 export type WishlistTemplateProps = {
 	recommendedGames: GameCardProps[]
-	recommendedHighLight: HighlightProps
+	recommendedHighlight: HighlightProps
+	games?: GameCardProps[]
 }
 
 const Wishlist = ({
 	recommendedGames,
-	recommendedHighLight
+	recommendedHighlight,
+	games
 }: WishlistTemplateProps) => (
 	<Base>
 		<Container>
 			<Heading lineLeft lineColor="secondary">
 				Wishlist
 			</Heading>
+
+			{games?.map((game, index) => (
+				<GameCard key={`wishlist-${index}`} {...game} />
+			))}
 		</Container>
+
+		<Showcase
+			title="You may like these games"
+			games={recommendedGames}
+			highlight={recommendedHighlight}
+		/>
 	</Base>
 )
 
--- src/templates/Wishlist/test.spec.tsx
+++ src/templates/Wishlist/test.spec.tsx
@@ -1,14 +1,33 @@
+import 'match-media-mock'
 import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import Wishlist from '.'
 
+import gamesMock from 'components/GameCardSlider/mock'
+import highlightMock from 'components/Highlight/mock'
+
+const props = {
+	games: gamesMock,
+	recommendedHighlight: highlightMock,
+	recommendedGames: gamesMock
+}
+
+jest.mock('components/Showcase', () => ({
+	__esModule: true,
+	default: function Mock() {
+		return <div data-testid="Mock Showcase" />
+	}
+}))
+
 describe('<Wishlist />', () => {
-	it('should render the heading', () => {
-		renderWithTheme(<Wishlist />)
+	it('should render correctly', () => {
+		renderWithTheme(<Wishlist {...props} />)
 
-		const heading = screen.getByRole('heading', { name: /Wishlist/i })
-
-		expect(heading).toBeInTheDocument()
+		expect(
+			screen.getByRole('heading', { name: /wishlist/i })
+		).toBeInTheDocument()
+		expect(screen.getByTestId('Mock Showcase')).toBeInTheDocument()
+		expect(screen.getAllByText(/population zero/i)).toHaveLength(6)
 	})
 })
