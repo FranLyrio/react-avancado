9b8d9d80370dffa461f3bdae430fe492194c307e
FranLyrio <lyriofranciele@gmail.com>
2022-07-13 14:34:17 -0300

    chore: tira dependência do babel

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
-module.exports = {
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const nextJest = require('next/jest')
+
+const createJestConfig = nextJest({
+	dir: './'
+})
+
+const customJestConfig = {
 	testEnvironment: 'jsdom',
 	testPathIgnorePatterns: ['/node_modules/', '/.next/'],
 	collectCoverage: true,
@@ -13,3 +20,5 @@ module.exports = {
 	setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
 	modulePaths: ['<rootDir>/src/', '<rootDir>/.jest']
 }
+
+module.exports = createJestConfig(customJestConfig)
