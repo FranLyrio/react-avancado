d362c5418f24cf5c6c007ac93167cdc1a37639b0
Franciele Lyrio Moraes <lyriofranciele@gmail.com>
2022-08-09 11:13:27 -0300

    test(CartDropdown): testa renderização das propriedades do componente

--- src/components/CartDropdown/test.spec.tsx
+++ src/components/CartDropdown/test.spec.tsx
@@ -1,3 +1,4 @@
+import { screen } from '@testing-library/react'
 import { renderWithTheme } from 'utils/tests/helpers'
 
 import items from 'components/CartList/mock'
@@ -5,7 +6,17 @@ import items from 'components/CartList/mock'
 import CartDropdown from '.'
 
 describe('<CartDropdown />', () => {
-	it('should render the heading', () => {
+	it('should render <CartIcon /> and its badge', () => {
 		renderWithTheme(<CartDropdown items={items} total="R$ 330,00" />)
+
+		expect(screen.getByLabelText(/shopping cart/i)).toBeInTheDocument()
+		expect(screen.getByText(`${items.length}`)).toBeInTheDocument()
+	})
+
+	it('should render Dropdown content with cart items and total', () => {
+		renderWithTheme(<CartDropdown items={items} total="R$ 330,00" />)
+
+		expect(screen.getByText('R$ 330,00')).toBeInTheDocument()
+		expect(screen.getByText(`${items[0].title}`)).toBeInTheDocument()
 	})
 })
